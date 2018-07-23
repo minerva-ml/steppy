cb8d3e5b13515c71624e49cd3f27cde6c3aad541
Kamil A. Kaczmarek <kamil.kaczmarek@neptune.ml>
2018-07-23 17:01:36 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,12 +21,12 @@
 
 project = 'steppy'
 copyright = '2018, neptune.ml'
-author = 'Kamil A. Kaczmarek'
+author = 'Kamil A. Kaczmarek and Jakub Czakon'
 
 # The short X.Y version
 version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.6'
 
 
 # -- General configuration ---------------------------------------------------
